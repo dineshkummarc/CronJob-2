80d2d8244cfca983264ee14be4acb9116566ccc8
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-09 09:12:11 +0530

    update

---
+++ imagesupload.php
@@ -0,0 +1,34 @@
+<?php
+
+// namespace CKSource\CKFinder\Plugin;
+
+
+
+// interface PluginInterface
+//  {
+//      public function setContainer(CKFinder $app);
+//      public function getDefaultConfig();
+//  }
+
+
+
+ //  public function getDefaultConfig()
+ // {
+ //     return array(
+ //         'imagePath' => '/path/stamp.png',
+ //         'position' => array(
+ //             'right' => 10,
+ //             'bottom' => 'center'
+ //         )
+ //     );
+ // }
+function retieve_data(){
+
+	echo "here";
+	print_r($_POST);
+	exit;
+}
+
+
+
+?>
